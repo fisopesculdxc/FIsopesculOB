2c35b40bb36fc27f57d00e508f24398c8c1ce9d2
fisopesculdxc <f.isopescul@dxc.com>
2024-08-05 09:29:33 +0300

    Problema1 Update

--- Problem1.c
+++ Problem1.c
@@ -148,28 +148,110 @@ void print_node(struct node* my_node)
     printf("%d ", my_node->val);
 }
 
+pthread_mutex_t lock;
+pthread_cond_t cond;
+int threads_ready = 0;
+
+void wait_for_all_threads()
+{
+    pthread_mutex_lock(&lock);
+    threads_ready++;
+    if (threads_ready < 3)
+    {
+        pthread_cond_wait(&cond, &lock);
+    }
+    else
+    {
+        pthread_cond_broadcast(&cond);
+    }
+    pthread_mutex_unlock(&lock);
+}
+
+void* thread_func1(void* arg)
+{
+    struct node** head = (struct node**)arg;
+    wait_for_all_threads();
+    pthread_mutex_lock(&lock);
+    printf("Thread %lu: add(2)\n", pthread_self());
+    add_node(head, 2, print_node);
+    printf("Thread %lu: add(4)\n", pthread_self());
+    add_node(head, 4, print_node);
+    printf("Thread %lu: add(10)\n", pthread_self());
+    add_node(head, 10, print_node);
+    printf("Thread %lu: delete(2)\n", pthread_self());
+    delete_node(head, 2);
+    printf("Thread %lu: sort_list()\n", pthread_self());
+    sort_list(head);
+    printf("Thread %lu: delete(10)\n", pthread_self());
+    delete_node(head, 10);
+    printf("Thread %lu: delete(5)\n", pthread_self());
+    delete_node(head, 5);
+    pthread_mutex_unlock(&lock);
+    return NULL;
+}
+
+void* thread_func2(void* arg)
+{
+    struct node** head = (struct node**)arg;
+    wait_for_all_threads();
+    pthread_mutex_lock(&lock);
+    printf("Thread %lu: add(11)\n", pthread_self());
+    add_node(head, 11, print_node);
+    printf("Thread %lu: add(1)\n", pthread_self());
+    add_node(head, 1, print_node);
+    printf("Thread %lu: delete(11)\n", pthread_self());
+    delete_node(head, 11);
+    printf("Thread %lu: add(8)\n", pthread_self());
+    add_node(head, 8, print_node);
+    printf("Thread %lu: print_list()\n", pthread_self());
+    print_list(head);
+    pthread_mutex_unlock(&lock);
+    return NULL;
+}
+
+void* thread_func3(void* arg)
+{
+    struct node** head = (struct node**)arg;
+    wait_for_all_threads();
+    pthread_mutex_lock(&lock);
+    printf("Thread %lu: add(30)\n", pthread_self());
+    add_node(head, 30, print_node);
+    printf("Thread %lu: add(25)\n", pthread_self());
+    add_node(head, 25, print_node);
+    printf("Thread %lu: add(100)\n", pthread_self());
+    add_node(head, 100, print_node);
+    printf("Thread %lu: sort_list()\n", pthread_self());
+    sort_list(head);
+    printf("Thread %lu: print_list()\n", pthread_self());
+    print_list(head);
+    printf("Thread %lu: delete(100)\n", pthread_self());
+    delete_node(head, 100);
+    pthread_mutex_unlock(&lock);
+    return NULL;
+}
+
 int main()
 {
     struct node* head = NULL;
-    add_node(&head, 5, print_node);
-    add_node(&head, 4, print_node);
-    add_node(&head, 3, print_node);
-    add_node(&head, 2, print_node);
-    add_node(&head, 1, print_node);
 
+    pthread_t thread1, thread2, thread3;
+    pthread_mutex_init(&lock, NULL);
+    pthread_cond_init(&cond, NULL);
+
+    pthread_create(&thread1, NULL, thread_func1, &head);
+    pthread_create(&thread2, NULL, thread_func2, &head);
+    pthread_create(&thread3, NULL, thread_func3, &head);
+
+    pthread_join(thread1, NULL);
+    pthread_join(thread2, NULL);
+    pthread_join(thread3, NULL);
+
+    printf("Final list:\n");
     print_list(&head);
 
-    //delete_node(&head, 4);
-    //print_list(&head);
-
-    //delete_node(&head, 3);
-    //print_list(&head);
-
-    //flush_list(&head);
-    //print_list(&head);
-
-    sort_list(&head);
-    print_list(&head);
+    pthread_mutex_destroy(&lock);
+    pthread_cond_destroy(&cond);
+    flush_list(&head);
 
     return 0;
 }
